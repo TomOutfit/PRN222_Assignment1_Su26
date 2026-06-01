5547c5510a010fe15f8307d7c2509afd0a79260c
Nguyễn Bình An <167419245+TomOutfit@users.noreply.github.com>
2026-06-01 21:33:27 +0700

    Fix: Change register Dependency from ITagService

--- NguyenBinhAn_A01_Business/Services/TagService.cs
+++ NguyenBinhAn_A01_Business/Services/TagService.cs
@@ -1,3 +1,4 @@
+using Microsoft.Extensions.DependencyInjection;
 using NguyenBinhAn_A01_Data.Models;
 using NguyenBinhAn_A01_Business.Repositories;
 
@@ -5,35 +6,45 @@ namespace NguyenBinhAn_A01_Business.Services
 {
     public class TagService : ITagService
     {
-        private readonly ITagRepository _tagRepository;
+        private readonly IServiceScopeFactory _scopeFactory;
 
-        public TagService(ITagRepository tagRepository)
+        public TagService(IServiceScopeFactory scopeFactory)
         {
-            _tagRepository = tagRepository;
+            _scopeFactory = scopeFactory;
         }
 
         public async Task<IEnumerable<Tag>> GetAllTagsAsync()
         {
+            using var scope = _scopeFactory.CreateScope();
+            var _tagRepository = scope.ServiceProvider.GetRequiredService<ITagRepository>();
             return await _tagRepository.GetAllAsync();
         }
 
         public async Task<Tag?> GetTagByIdAsync(int id)
         {
+            using var scope = _scopeFactory.CreateScope();
+            var _tagRepository = scope.ServiceProvider.GetRequiredService<ITagRepository>();
             return await _tagRepository.GetByIdAsync(id);
         }
 
         public async Task<Tag?> GetTagByNameAsync(string name)
         {
+            using var scope = _scopeFactory.CreateScope();
+            var _tagRepository = scope.ServiceProvider.GetRequiredService<ITagRepository>();
             return await _tagRepository.GetByNameAsync(name);
         }
 
         public async Task<Tag> CreateTagAsync(Tag tag)
         {
+            using var scope = _scopeFactory.CreateScope();
+            var _tagRepository = scope.ServiceProvider.GetRequiredService<ITagRepository>();
             return await _tagRepository.AddAsync(tag);
         }
 
         public async Task<Tag> UpdateTagAsync(Tag tag)
         {
+            using var scope = _scopeFactory.CreateScope();
+            var _tagRepository = scope.ServiceProvider.GetRequiredService<ITagRepository>();
             await _tagRepository.UpdateAsync(tag);
             return tag;
         }
@@ -42,6 +53,8 @@ namespace NguyenBinhAn_A01_Business.Services
         {
             if (await CanDeleteTagAsync(id))
             {
+                using var scope = _scopeFactory.CreateScope();
+                var _tagRepository = scope.ServiceProvider.GetRequiredService<ITagRepository>();
                 var tag = await _tagRepository.GetByIdAsync(id);
                 if (tag != null)
                 {
@@ -56,11 +69,15 @@ namespace NguyenBinhAn_A01_Business.Services
 
         public async Task<IEnumerable<Tag>> GetPopularTagsAsync(int limit = 10)
         {
+            using var scope = _scopeFactory.CreateScope();
+            var _tagRepository = scope.ServiceProvider.GetRequiredService<ITagRepository>();
             return await _tagRepository.GetPopularTagsAsync(limit);
         }
 
         public async Task<bool> CanDeleteTagAsync(int tagId)
         {
+            using var scope = _scopeFactory.CreateScope();
+            var _tagRepository = scope.ServiceProvider.GetRequiredService<ITagRepository>();
             return !(await _tagRepository.IsTagInUseAsync(tagId));
         }
     }
